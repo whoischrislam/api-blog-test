e69561a5c40603ef2b996572ea8d5956a907e42d
whoischrislam <whoischrislam@gmail.com>
2024-03-05 07:28:39 -1000

    fetch calls update

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const body = document.body;
+const postAction = document.getElementById("postAction");
 const container = document.createElement("div");
+container.id = "container";
 body.appendChild(container);
 
 fetch("https://apis.scrimba.com/jsonplaceholder/posts")
@@ -21,3 +23,39 @@ fetch("https://apis.scrimba.com/jsonplaceholder/posts")
             container.append(postContainer);
         });
     });
+
+postAction.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const title = document.getElementById("postTitle");
+    const content = document.getElementById("postContent");
+    const post = {
+        title: title.value,
+        body: content.value
+    };
+    const options = {
+        method: "POST",
+        body: JSON.stringify(post),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    };
+    fetch("https://apis.scrimba.com/jsonplaceholder/posts", options)
+        .then((response) => response.json())
+        .then((data) => {
+            const postContainer = document.createElement("div");
+            postContainer.id = "postContainer";
+            const title = document.createElement("h1");
+            const body = document.createElement("p");
+            const bigContainer = document.getElementById("container");
+            title.textContent =  `Title: ${post.title}`;
+            body.textContent = `Text: ${post.body}`;
+            title.id = "title";
+            body.id = "blogBody";
+            postContainer.append(title, body);
+            postContainer.append(document.createElement("hr"));
+            const recentPost = bigContainer.firstChild;
+            bigContainer.insertBefore(postContainer, recentPost);
+        });
+    title.value = "";
+    content.value = "";
+});
